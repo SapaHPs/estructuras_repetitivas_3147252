b88fca3058efe0fa3efeb1a2cd806a390dde44c9
SapaHPs <juanitomm2408@gmail.com>
2025-04-04 04:37:19 +1200

    Create ciclo_while.py

---
+++ ciclo_while.py
@@ -0,0 +1,13 @@
+#Ejemplo 1
+#Elavore un ciclo para 
+#mostrar los numeros de
+#1 al 10 en python
+
+numero = 0
+
+while numero <= 5:
+    print (numero)
+    
+    numero = numero + 1 
+    
+print("Fin programa")
